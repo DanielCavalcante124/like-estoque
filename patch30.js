41d0f5376583f0f2ab45acaa3863bff0be31b792
DanielCavalcante124 <cavalcantedaniel04@gmail.com>
2026-06-04 14:51:36 -0300

    Carregar patch39 corrige botoes

--- patch30.js
+++ patch30.js
@@ -3,7 +3,7 @@ function load(id,src){if(document.getElementById(id))return;var s=document.creat
 document.addEventListener('DOMContentLoaded',function(){
  setTimeout(function(){load('patch37loader','patch37.js?v=37')},1200);
  setTimeout(function(){load('patch38loader','patch38.js?v=38')},1800);
- setTimeout(function(){load('patch37loader','patch37.js?v=37')},3000);
- setTimeout(function(){load('patch38loader','patch38.js?v=38')},3600);
+ setTimeout(function(){load('patch39loader','patch39.js?v=39')},2200);
+ setTimeout(function(){load('patch39loader','patch39.js?v=39')},4200);
 });
 })();
