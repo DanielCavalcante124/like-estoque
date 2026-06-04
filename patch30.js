8c87653865e46a3a7e0e4c232dd6d4a7436ddb2c
DanielCavalcante124 <cavalcantedaniel04@gmail.com>
2026-06-04 15:24:22 -0300

    Corrigir PDF e oscilacao baixa autorizada

--- patch30.js
+++ patch30.js
@@ -4,8 +4,7 @@ document.addEventListener('DOMContentLoaded',function(){
  setTimeout(function(){load('patch37loader','patch37.js?v=37')},1200);
  setTimeout(function(){load('patch38loader','patch38.js?v=38')},1800);
  setTimeout(function(){load('patch39loader','patch39.js?v=39')},2200);
- setTimeout(function(){load('patch40loader','patch40.js?v=40')},2600);
- setTimeout(function(){load('patch41loader','patch41.js?v=41')},3000);
- setTimeout(function(){load('patch41loader','patch41.js?v=41')},5000);
+ setTimeout(function(){load('patch42loader','patch42.js?v=42')},2600);
+ setTimeout(function(){load('patch42loader','patch42.js?v=42')},5000);
 });
 })();
