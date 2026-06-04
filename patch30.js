2c98194417fdcdc1246046788733f8aa84b41ea2
DanielCavalcante124 <cavalcantedaniel04@gmail.com>
2026-06-04 15:03:43 -0300

    Carregar melhoria baixa autorizada

--- patch30.js
+++ patch30.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded',function(){
  setTimeout(function(){load('patch37loader','patch37.js?v=37')},1200);
  setTimeout(function(){load('patch38loader','patch38.js?v=38')},1800);
  setTimeout(function(){load('patch39loader','patch39.js?v=39')},2200);
+ setTimeout(function(){load('patch40loader','patch40.js?v=40')},2600);
  setTimeout(function(){load('patch39loader','patch39.js?v=39')},4200);
+ setTimeout(function(){load('patch40loader','patch40.js?v=40')},4600);
 });
 })();
