b71cd6d5097464b821faa46ef69178b169937714
DanielCavalcante124 <cavalcantedaniel04@gmail.com>
2026-06-05 09:15:43 -0300

    Carregar entrada por quantidade

--- patch30.js
+++ patch30.js
@@ -13,5 +13,6 @@ document.addEventListener('DOMContentLoaded',function(){
  setTimeout(function(){load('patch51loader','patch51.js?v=56')},5200);
  setTimeout(function(){load('patch56loader','patch56.js?v=56')},6100);
  setTimeout(function(){load('patch60loader','patch60.js?v=60')},6800);
+ setTimeout(function(){load('patch61loader','patch61.js?v=61')},7400);
 });
 })();
