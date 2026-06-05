aeca921aff8d239c25e6e312e9fe5e3aaff63251
DanielCavalcante124 <cavalcantedaniel04@gmail.com>
2026-06-05 01:11:25 -0300

    Atualizar cache dos modulos finais

--- patch30.js
+++ patch30.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded',function(){
  setTimeout(function(){load('patch44loader','patch44.js?v=44')},3600);
  setTimeout(function(){load('patch46loader','patch46.js?v=46')},4200);
  setTimeout(function(){load('patch50loader','patch50.js?v=50')},4700);
- setTimeout(function(){load('patch51loader','patch51.js?v=51')},5200);
- setTimeout(function(){load('patch51loader','patch51.js?v=51')},7300);
+ setTimeout(function(){load('patch51loader','patch51.js?v=56')},5200);
+ setTimeout(function(){load('patch56loader','patch56.js?v=56')},6100);
 });
 })();
