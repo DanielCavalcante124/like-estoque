34633bba849b09957ce2aae212d730e0429e2b7c
DanielCavalcante124 <cavalcantedaniel04@gmail.com>
2026-06-05 14:44:09 -0300

    Remover carregamento antigo de materiais

--- patch30.js
+++ patch30.js
@@ -14,6 +14,5 @@ document.addEventListener('DOMContentLoaded',function(){
  setTimeout(function(){load('patch60loader','patch60.js?v=60')},6100);
  setTimeout(function(){load('patch61loader','patch61.js?v=61')},6800);
  setTimeout(function(){load('patch62loader','patch62.js?v=62')},7300);
- setTimeout(function(){load('patch63loader','patch63.js?v=63')},7900);
 });
 })();
